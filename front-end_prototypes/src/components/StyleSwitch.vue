9a88ae8ce6a41210c0332c5b46f6c1b0e3afd9d6
Odei Maiz <maiz@zurichmedtech.com>
2017-10-30 10:24:41 +0100

    vue prototype: working version

---
+++ front-end_prototypes/src/components/StyleSwitch.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <label>Day</label>
+    <input type="radio" value=0 v-model="internalNightSelected">
+    <input type="radio" value=1 v-model="internalNightSelected">
+    <label>Night</label>
+  </div>
+</template>
+
+<script>
+
+import EventBus from './event-bus'
+
+export default {
+  name: 'StyleSwitch',
+  props: ['nightSelected'],
+  data () {
+    return {
+      internalNightSelected: 0
+    }
+  },
+  watch: {
+    'internalNightSelected': function () {
+      EventBus.$emit('nightSelectionChanged', this.internalNightSelected)
+    }
+  },
+  created () {
+    this.internalNightSelected = this.nightSelected
+  }
+}
+</script>
