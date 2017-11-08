313c5a05d2c7670f7d44471017e1d9dafa36b8f5
Pedro Crespo-Valero <crespo@speag.com>
2017-11-08 09:58:38 +0100

    dockerize_react: adapted to new folder layout. Now all front-end_prototypes/react -> demos/frontend-react

--- front-end_prototypes/react/app/src/socket2Server.js
+++
@@ -1,3 +0,0 @@
-import openSocket from 'socket.io-client';
-const socket = openSocket('http://172.16.9.47:6002');
-export { socket };
--- front-end_prototypes/react/app/src/store.js
+++
@@ -1,7 +0,0 @@
-import { applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
-import { createLogger } from 'redux-logger';
-import allReducers from './reducers';
-
-const middleware = applyMiddleware(thunk, createLogger());
-export default createStore(allReducers, middleware);
