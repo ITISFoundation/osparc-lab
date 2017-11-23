7ea798c5c1df96771bc1802302c6c15110de044a
odeimaiz <maiz@zurichmedtech.com>
2017-11-23 11:41:20 +0100

    layout with three panes: - Settings - Workbench - Results  - Folder  - Viewer

--- demos/frontend-qooxdoo/app/source/class/app/Application.js
+++ demos/frontend-qooxdoo/app/source/class/app/Application.js
@@ -2,9 +2,9 @@
 
    Copyright:
 
-   License:
+   License: MIT
 
-   Authors:
+   Authors: maiz@itis.ethz.com
 
 ************************************************************************ */
 
@@ -28,9 +28,9 @@ qx.Class.define("app.Application",
   members :
   {
     /**
-     * This method contains the initial application code and gets called 
+     * This method contains the initial application code and gets called
      * during startup of the application
-     * 
+     *
      * @lint ignoreDeprecated(alert)
      */
     main : function()
@@ -53,19 +53,92 @@ qx.Class.define("app.Application",
       -------------------------------------------------------------------------
       */
 
-      // Create a button
-      var button1 = new qx.ui.form.Button("First Button", "app/test.png");
+      this._createSettingsWindow(20, 20, 350, 450);
+      this._createResultsViewerWindow(1000, 20, 800, 800);
+      this._createWorkbenchWindow(20, 550, 800, 400);
+    },
 
-      // Document is the application root
-      var doc = this.getRoot();
-
-      // Add button to document at fixed coordinates
-      doc.add(button1, {left: 100, top: 50});
-
-      // Add an event listener
-      button1.addListener("execute", function(e) {
-        alert("Hello World!");
+    _createSettingsWindow : function(left, top, width, height)
+    {
+      var win = new qx.ui.window.Window("Settings").set({
+        contentPadding: 0,
+        width: width,
+        height: height
       });
+      win.setLayout(new qx.ui.layout.Grow());
+
+      var scroller = new qx.ui.container.Scroll();
+      win.add(scroller);
+
+      var container = new qx.ui.container.Composite(new qx.ui.layout.Flow()).set({
+        allowShrinkY: false
+      });
+      scroller.add(container);
+
+      win.moveTo(left, top);
+      win.open();
+    },
+
+    _createResultsViewerWindow : function(left, top, width, height)
+    {
+      var win = new qx.ui.window.Window("Results").set({
+        contentPadding: 0,
+        width: width,
+        height: height
+      });
+      win.setLayout(new qx.ui.layout.Grow());
+
+      var scroller = new qx.ui.container.Scroll();
+      win.add(scroller);
+
+      var container = new qx.ui.container.Composite(new qx.ui.layout.Flow()).set({
+        allowShrinkY: false
+      });
+      scroller.add(container);
+
+
+      var splitpane = new qx.ui.splitpane.Pane("horizontal");
+      // splitpane.setWidth(400);
+      // splitpane.setHeight(60);
+      splitpane.setDecorator("main");
+      // this.getRoot().add(splitpane, {left:20, top:20});
+      win.add(splitpane);
+
+      // Left
+      var leftWidget = new qx.ui.form.TextArea("Folder");
+      leftWidget.setDecorator(null);
+      leftWidget.setWrap(true);
+      splitpane.add(leftWidget, 1);
+
+      // Right
+      var rightWidget = new qx.ui.form.TextArea("Viewer");
+      rightWidget.setDecorator(null);
+      rightWidget.setWrap(true);
+      splitpane.add(rightWidget, 2);
+
+      win.moveTo(left, top);
+      win.open();
+    },
+
+    _createWorkbenchWindow : function(left, top, width, height)
+    {
+      var win = new qx.ui.window.Window("Workbench").set({
+        contentPadding: 0,
+        width: width,
+        height: height
+      });
+      win.setLayout(new qx.ui.layout.Grow());
+
+      var scroller = new qx.ui.container.Scroll();
+      win.add(scroller);
+
+      var container = new qx.ui.container.Composite(new qx.ui.layout.Flow()).set({
+        allowShrinkY: false
+      });
+      scroller.add(container);
+
+      win.moveTo(left, top);
+      win.open();
     }
   }
 });
