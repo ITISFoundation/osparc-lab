8993c7211517695b20d8b5593bb07428cbbf55ea
odeimaiz <maiz@zurichmedtech.com>
2017-11-24 16:36:21 +0100

    qooxdoo prototype created using qx-cli

---
+++ demos/frontend-qooxdoo/app2/source/class/app2/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("app2.theme.Theme",
+{
+  meta :
+  {
+    color : app2.theme.Color,
+    decoration : app2.theme.Decoration,
+    font : app2.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : app2.theme.Appearance
+  }
+});
