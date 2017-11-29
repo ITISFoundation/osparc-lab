8f114b7be113f1ca5079f85c3c574d7273ca0592
Manuel Guidon <guidon@itis.ethz.ch>
2017-11-29 10:21:18 +0100

    combine input and outputhash and query db

--- demos/workflow/director/director.py
+++ demos/workflow/director/director.py
@@ -15,10 +15,17 @@ def nice_json(arg):
     response.headers['Content-type'] = "application/json"
     return response
 
+def output_exists(output_hash):
+    db_client = MongoClient("mongodb://database:27017/")
+    output_database = db_client.output_database
+    output_collections = output_database.output_collections
+    exists = output_collections.find_one({"_hash" : str(output_hash)})
+    return not exists is None
+   
 def parse_input_data(data):
     if data.has_key("input"):
         inp = data["input"]
-        data_hash = hashlib.md5(json.dumps(inp, sort_keys=True)).hexdigest()
+        data_hash = hashlib.sha256(json.dumps(inp, sort_keys=True)).hexdigest()
         db_client = MongoClient("mongodb://database:27017/")
         input_database = db_client.input_database
         input_collections = input_database.input_collections
@@ -27,7 +34,7 @@ def parse_input_data(data):
            data["_hash"] = [str(data_hash)]
            input_collections.insert_one(data)
     
-        return [str(data_hash), not exists is None]
+        return [data_hash, not exists is None]
 
 def parse_container_data(data):
     if data.has_key("container"):
@@ -37,10 +44,8 @@ def parse_container_data(data):
         client = docker.from_env(version='auto')
         client.login(registry="masu.speag.com/v2", username="z43", password="z43")
         img = client.images.pull(container_name, tag=container_tag)
-        
-
-
-        return [str(img.id), False]
+        container_hash = str(img.id).split(':')[1]
+        return container_hash
 
 
 @app.route("/services", methods=['GET'])
@@ -61,10 +66,19 @@ def run_pipeline():
     hashstr = ""
     [input_hash, input_exists] = parse_input_data(data)
    
-    [output_hash, output_exists] = parse_container_data(data)
+    container_hash = parse_container_data(data)
 
-    return nice_json({"input_hash" : input_hash, "input_exists" : str(input_exists),
-        "output_hash" : output_hash, "output_exists" : str(output_exists)})
+    combined = hashlib.sha256()
+    combined.update(input_hash)
+    combined.update(container_hash)
+    output_hash = combined.hexdigest()
+
+    output_ready = output_exists(output_hash)
+    if not output_ready:
+       print "needs some work" 
+
+    return nice_json({"input_hash" : str(input_hash), "input_exists" : str(input_exists),
+        "output_hash" : str(output_hash), "output_exists" : str(output_ready)})
 
 
 if __name__ == "__main__":
