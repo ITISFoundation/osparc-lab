1ccf922014b4fa0ffa666775fdf926e3aac316f1
Stefan Benkler <benkler@zurichmedtech.com>
2017-12-05 14:49:11 +0100

    Setup CI #24 - added check of targets within Makefile ... not final, needs to make sure, they make sense to be always present

--- tools/TestDemosFileStructure.py
+++ tools/TestDemosFileStructure.py
@@ -9,6 +9,9 @@ class AllFine:
 		print '[ERROR]: ' + msg
 		self.__ok = False
 		
+	def Warning( self, msg ):
+		print '[WARNING]: ' + msg
+		
 	def Okay( self ):
 		if self.__ok:
 			print 'All tests passed'
@@ -16,6 +19,28 @@ class AllFine:
 			self.Error( 'Some tests failed' )
 		return self.__ok
 
+		
+def CheckMakefile( folder, all_fine ):
+	print 'Checking makefile'
+	has_demo = False
+	has_start = False
+	has_stop = False
+	with open( os.path.join( folder, 'Makefile' ) ) as f:
+		for line in f:
+			if line.startswith( 'demo:' ):
+				has_demo = True
+			if line.startswith( 'start:' ):
+				has_start = True
+			if line.startswith( 'stop:' ):
+				has_stop = True
+				
+	if not has_demo:
+		all_fine.Warning( 'No "demo" target' )
+	if not has_start:
+		all_fine.Warning( 'No "start" target' )
+	if not has_stop:
+		all_fine.Warning( 'No "stop" target' )
+		
 def TestFileStructure():
 
 	print 'Testing file structure in demos folder'
@@ -27,10 +52,19 @@ def TestFileStructure():
 		folder = os.path.join( root_folder, sub_folder )
 		if os.path.isdir( folder ):
 			print 'Testing folder: ' + folder
+			
+			# Makefile checks
 			if not os.path.exists( os.path.join( folder, 'Makefile' ) ):
 				all_fine.Error( 'File "Makefile" does not exist (case sensitive?)' )
+			else:
+				CheckMakefile( folder, all_fine )
+			
+			# Readme check
 			if not os.path.exists( os.path.join( folder, 'README.md' ) ):
 				all_fine.Error( 'File "README.md" does not exist (case sensitive?)' )
+			else:
+				if 0 == len( file( os.path.join( folder, 'README.md' ) ).readlines() ):
+					all_fine.Error( 'Readme file seems empty' )
 			
 	return all_fine.Okay()
 
