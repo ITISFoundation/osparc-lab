343f894230c41501e2c7380eb7b428d08e00ef5f
Manuel Guidon <guidon@itis.ethz.ch>
2017-12-20 13:36:43 +0100

    sync with simcore-dev ps

--- demos/workflow/scripts/deploy.sh
+++ demos/workflow/scripts/deploy.sh
@@ -1,1 +1,2 @@
-docker login -u z43 -p z43 masu.speag.com && docker stack deploy -c docker-compose-swarm.yml workflow --with-registry-auth
+docker-machine scp docker-compose-swarm.yml manager1:
+docker-machine ssh manager1 'docker login -u z43 -p z43 masu.speag.com && docker stack deploy -c docker-compose-swarm.yml workflow --with-registry-auth'
--- demos/workflow/scripts/do_forward.sh
+++ demos/workflow/scripts/do_forward.sh
@@ -2,5 +2,5 @@
 ./pf 8081 -e manager1
 ./pf 15672 -e manager1
 ./pf 5555 -e manager1
+./pf 8010 -e manager1
 ./pf 8010 -e worker1
-
