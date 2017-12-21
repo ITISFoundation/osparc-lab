af41320f593cddeb8faae4579d4c100ca420e144
pcrespov <32402063+pcrespov@users.noreply.github.com>
2017-12-21 17:05:25 +0100

    Feature/front end wt (#47)
    * front-end wt: basic demo with tea-pot. Builds with visual studio in win and with cmake in linux
    
    * front-end wt: cleaned up docker and doc
    
    * added missing ignore list files
    
    * Updated doc
    
    * front-end wt: minor fix in docker and added demo, start, stop commands in cmake

---
+++ demos/frontend-wt/WTSparcSandbox/sessionview/NavigationBar.cpp
@@ -0,0 +1,14 @@
+#include "NavigationBar.h"
+
+namespace sparc
+{
+	NavigationBar::NavigationBar()
+	{
+		// See http://www.webtoolkit.eu/widgets/navigation/navigation-bar
+
+		setTitle("oSparc",
+			"http://www.google.com/search?q=oSparc");
+
+		setResponsive(true);
+	}
+}
