f84e9e78f690a063c51c4691b9a227d6a288036f
Pedro Crespo-Valero <pedro.crespovalero@gmail.com>
2018-02-26 09:19:55 +0100

    started python server

--- templates/boilerplate/server-python-tornado/app.py
+++ templates/boilerplate/server-python-tornado/app.py
@@ -12,7 +12,7 @@ import tornado.log
 import tornado.options
 from tornado.options import define, options
 
-define("port", default=8000, type=int)
+define("port", default=8080, type=int)
 
 _CFILE = os.path.abspath( sys.argv[0] if __name__ == "__main__" else __file__ )
 _CDIR = os.path.dirname( _CFILE)
@@ -24,6 +24,7 @@ ACCESS_LOG = logging.getLogger("tornado.access")
 APP_LOG = logging.getLogger("tornado.application")
 GEN_LOG = logging.getLogger("tornado.general")
 
+_CLIENT_DIR = os.path.normpath( os.path.join(_CDIR, "../client-qx/source-output") )
 
 # LIB ---
 
@@ -33,66 +34,34 @@ GEN_LOG = logging.getLogger("tornado.general")
 # REQUEST HANDLERS-------
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        #self.render('app/index.html')
-        self.write( open(os.path.join(_CDIR, 'source-output/app/index.html')).read() )
+        self.write( open(os.path.join(_CLIENT_DIR,'index.html')).read() )
 
-class CartHandler(tornado.web.RequestHandler):
-    def post(self):
-        """ Adds/removes book from cart """
-        action = self.get_argument('action', None)
-        session = self.get_argument('session', None)
-        print 'post.CartHandler :', action, session
-
-        if session is None:
-            self.set_status(400, "Could not identify session")
-            return
-
-        if action == 'add':
-            self.application.shopping_cart.move_item_to_cart(session)
-        elif action == 'remove':
-            self.application.shopping_cart.delete_item_from_cart(session)
-        else:
-            self.set_status(400, "Could not idenfity action")
-        return
-
-class StatusHandler(tornado.websocket.WebSocketHandler):
-    """
-        On document creation, the client opens the websocket channel
-        then, the application subscribes callbacks that
-        broadcast back to all subscribers via web-sockets
-    """
+class EchoWebSocket(tornado.websocket.WebSocketHandler):
     def open(self):
-        self.application.shopping_cart.subscribe(self._callback)
+        print("WebSocket opened")
+
+    def on_message(self, message):
+        self.write_message(u"You said: " + message)
 
     def on_close(self):
-        self.application.shopping_cart.unsubscribe(self._callback)
-
-    def on_message(self):
-        """
-            There are no messages comming from the client via the WebSocket
-            instead, there is a HTTP post request to Cart and
-            the server responds via the web-socket!!
-
-
-        """
-        pass
-    def _callback(self, count):
-        self.write_message( json.dumps(dict(inventoryCount=count)) )
+        print("WebSocket closed")
 
 
 # APPLICATION ---
 class Application(tornado.web.Application):
+    """
+        - Careful client-qx hardcodes websockets via port 8080
+    """
     def __init__(self):
         handlers = [
-            #(r"/(.*)", tornado.web.StaticFileHandler, {"path": "app/"}),
-            #(r'/', MainHandler),
-            #(r'/cart', CartHandler),
-            #(r'/cart/status', StatusHandler)
+            (r'/', MainHandler),
+            (r"/(.*)", tornado.web.StaticFileHandler, {"path":_CLIENT_DIR}),
+            (r'/socket.io/', EchoWebSocket)
         ]
 
         settings = dict(
-            template_path = os.path.join(_CDIR, 'template'),
-            static_path = os.path.join(_CDIR, 'source-output'),
+            #template_path = os.path.join(_CDIR, 'template'),
+            #static_path = os.path.join(_CDIR, 'static'),
             #static_handler_args=dict(default_filename="app/index.html"),
             debug=True,
             )
