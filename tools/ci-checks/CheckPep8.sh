2f42d5d83c85b891e7bf68a4bb7c55b230eca001
Pedro Crespo-Valero <pedro.crespovalero@gmail.com>
2018-03-31 16:35:57 +0200

    Fixed lint errors in tools/ scripts

--- tools/ci-checks/CheckPep8.sh
+++ tools/ci-checks/CheckPep8.sh
@@ -1,12 +1,12 @@
 # PC: disabled import-error because in this configuration, requirements are not installed!
 # PC: disabled fixme to allow TODO messages
 
-echo 'Pylint report legend -----------------------------------------------'
-echo '[R]efactor for a “good practice” metric violation'
-echo '[C]onvention for coding standard violation'
-echo '[W]arning for stylistic problems, or minor programming issues'
-echo '[E]rror for important programming issues (i.e. most probably bug)'
-echo '[F]atal for errors which prevented further processing'
-echo '--------------------------------------------------------------------'
+#'Pylint report legend -----------------------------------------------'
+#'[R]efactor for a “good practice” metric violation'
+#'[C]onvention for coding standard violation'
+#'[W]arning for stylistic problems, or minor programming issues'
+#'[E]rror for important programming issues (i.e. most probably bug)'
+#'[F]atal for errors which prevented further processing'
+#'--------------------------------------------------------------------'
 
 find $1 -iname "*.py" | xargs pylint --rcfile=.pylintrc --disable=import-error --disable=fixme
