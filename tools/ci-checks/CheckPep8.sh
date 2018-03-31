3b7e665ead516b9d66742002c9f391f75d32a663
Pedro Crespo-Valero <pedro.crespovalero@gmail.com>
2018-03-31 15:47:44 +0200

    Tuned up pylint check in travis

--- tools/ci-checks/CheckPep8.sh
+++ tools/ci-checks/CheckPep8.sh
@@ -1,1 +1,12 @@
-find $1 -iname "*.py" | xargs pylint --reports=no
+# PC: disabled import-error because in this configuration, requirements are not installed!
+# PC: disabled fixme to allow TODO messages
+
+echo 'Pylint report legend -----------------------------------------------'
+echo '[R]efactor for a “good practice” metric violation'
+echo '[C]onvention for coding standard violation'
+echo '[W]arning for stylistic problems, or minor programming issues'
+echo '[E]rror for important programming issues (i.e. most probably bug)'
+echo '[F]atal for errors which prevented further processing'
+echo '--------------------------------------------------------------------'
+
+find $1 -iname "*.py" | xargs pylint --rcfile=.pylintrc --disable=import-error --disable=fixme
