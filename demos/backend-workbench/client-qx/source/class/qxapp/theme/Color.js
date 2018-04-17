da949f62496cb3dc07b15ddfec79c314476691d8
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-04-17 17:54:02 +0200

    added backworkbench demo initial

---
+++ demos/backend-workbench/client-qx/source/class/qxapp/theme/Color.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright: 2018 
+
+   License: MIT license
+
+   Authors: 
+
+************************************************************************ */
+
+qx.Theme.define("qxapp.theme.Color",
+{
+  extend : qx.theme.indigo.Color,
+
+  colors :
+  {
+  }
+});
