f9db64b54d748c2afee93f0c54e053029c23cf07
sanderegg <35365065+sanderegg@users.noreply.github.com>
2018-04-23 13:20:11 +0200

    now runs in swarm or locally using LOCAL_MODE env variable fixed issues with accessing env variables now able to spawn services on swarm

--- demos/backend-workbench/director/producer.py
+++ demos/backend-workbench/director/producer.py
@@ -3,6 +3,10 @@ import registry_proxy
 import json
 import docker
 
+def isInLocalMode():
+    local_mode = bool(os.environ.get('LOCAL_MODE', True) == 'True')
+    return local_mode
+
 def start_service(service_name, service_tag, service_uuid):
     # get the repos implied by the service name
     listOfInteractiveServicesRepositories = registry_proxy.retrieve_list_of_repos_with_interactive_services()
@@ -17,12 +21,12 @@ def start_service(service_name, service_tag, service_uuid):
     dockerClient = docker.from_env()
     try:
         # login
-        registry_url = os.environ.get(REGISTRY_URL) + '/v2'
-        username = os.environ.get(REGISTRY_USER)
-        password = os.environ.get(REGISTRY_PW)
-        dockerClient.login(registry=registry_url, username=REGISTRY_USER, password=REGISTRY_PW)
+        registry_url = os.environ.get('REGISTRY_URL')
+        username = os.environ.get('REGISTRY_USER')
+        password = os.environ.get('REGISTRY_PW')
+        dockerClient.login(registry=registry_url + '/v2', username=username, password=password)        
     except docker.errors.APIError as e:
-        raise Exception('Error while loging to registry: ' + e)
+        raise Exception('Error while loging to registry: ' + str(e))
 
     
     listOfContainerIDs = []
@@ -36,7 +40,7 @@ def start_service(service_name, service_tag, service_uuid):
             tag = service_tag
         
         try:
-            dockerImageFullPath = REGISTRY_URL + dockerImagePath + ':' + tag
+            dockerImageFullPath = registry_url +'/' + dockerImagePath + ':' + tag
             # run the docker image
             if isInLocalMode():
                 container = dockerClient.containers.run(dockerImageFullPath, detach=True, auto_remove=True, labels={"uuid":service_uuid})            
@@ -49,11 +53,11 @@ def start_service(service_name, service_tag, service_uuid):
         except docker.errors.ImageNotFound as e:
             # first cleanup
             stop_service(service_uuid)
-            raise Exception('Error service not found: ' + e)
+            raise Exception('Error service not found: ' + str(e))
         except docker.errors.APIError as e:
             # first cleanup
             stop_service(service_uuid)            
-            raise Exception('Error while accessing docker server: ' + e)
+            raise Exception('Error while accessing docker server: ' + str(e))
     
     return json.dumps({'container_ids': listOfContainerIDs})
 
@@ -62,12 +66,12 @@ def stop_service(service_uuid):
     dockerClient = docker.from_env()
     try:
         # login
-        registry_url = os.environ.get(REGISTRY_URL) + '/v2'
-        username = os.environ.get(REGISTRY_USER)
-        password = os.environ.get(REGISTRY_PW)
+        registry_url = os.environ.get('REGISTRY_URL') + '/v2'
+        username = os.environ.get('REGISTRY_USER')
+        password = os.environ.get('REGISTRY_PW')
         dockerClient.login(registry=registry_url, username=username, password=password)
     except docker.errors.APIError as e:
-        raise Exception('Error while logging to registry: ' + e)
+        raise Exception('Error while logging to registry: ' + str(e))
     
     try:
         # get list of running containers and stop them
@@ -76,8 +80,8 @@ def stop_service(service_uuid):
             [container.stop() for container in listOfRunningContainersWithUUID]
         else:
             listOfRunningServicesWithUUID = dockerClient.services.list(filters={'label':'uuid=' + service_uuid})
-            [service.stop() for service in listOfRunningServicesWithUUID]
+            [service.remove() for service in listOfRunningServicesWithUUID]
 
     except docker.errors.APIError as e:
-        raise Exception('Error while stopping container' + e)
+        raise Exception('Error while stopping container' + str(e))
     
